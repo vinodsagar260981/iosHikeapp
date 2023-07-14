ffbd5f22f374ebe6e3ea7042b66cdb7adbf985a7
vinod sagar <vinodsagarshet@gmail.com>
2023-07-14 21:19:07 +0530

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Vinod Sagar on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
